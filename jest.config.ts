4b51473ad6a01f6243ec8ac3dbd1c25f1447da75
= <=>
2024-02-19 21:50:14 -0300

    Atualizações Libs - 19022150

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "<rootDir>/style-mock.js",
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@/services/(.*)$': '<rootDir>/src/services/$1',
+    '^@/contexts/(.*)$': '<rootDir>/src/contexts/$1',
+  },
+};
+
+export default config;
